254a0bbf877b0860fc391fc851c586a27acc1e1a
Mads Akselsen <akselsenmads@gmail.com>
2021-12-30 15:53:00 +0100

    react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 
 function App() {
     return (
         <div>
-            <HomePage />
+            <Switch>
+                {/* Note: when a component is added to the component={myComponent} it gets passed three arguments: history, location and match  */}
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/topics" component={HomePage} />
+                <Route exact path="/topics/:topId" component={HomePage} />
+            </Switch>
         </div>
     );
 }
