f53ae1233b433123a485baa5b319bfa972c2174b
Mads Akselsen <akselsenmads@gmail.com>
2022-01-05 08:01:38 +0100

    form sign-in

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 const HatsPage = () => (
     <div>
@@ -19,8 +20,8 @@ function App() {
             <Switch>
                 {/* Note: when a component is added to the component={myComponent} it gets passed three arguments: history, location and match  */}
                 <Route exact path="/" component={HomePage} />
-                <Route exact path="/shop" component={ShopPage} />
-                <Route exact path="/shop/:topId" component={HatsPage} />
+                <Route path="/shop" component={ShopPage} />
+                <Route path="/signin" component={SignInAndSignUpPage} />
             </Switch>
         </div>
     );
