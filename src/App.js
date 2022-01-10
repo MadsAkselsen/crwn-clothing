822662fc406e264974f440ee2779af2060b17496
Mads Akselsen <akselsenmads@gmail.com>
2022-01-10 08:53:45 +0100

    setting user in database

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
     constructor() {
@@ -21,7 +21,9 @@ class App extends React.Component {
 
     componentDidMount() {
         // auth.onAuthStateChanged returns a function for unsubscribing
-        this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
+            createUserProfileDocument(user);
+
             this.setState({ currentUser: user });
 
             console.log(user);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,52 @@ const config = {
     appId: '1:51292091519:web:4f1d9ca148e23e1630bb4a',
 };
 
+// QueryReference: is what get we by calling, e.g. firestore.doc('users/:userId)
+// the queryReference does not have the actual data og the collection or document.
+// It instead have properties that tell us details about it, or the method
+// to get the Snapshot object which gives us the data we are looking for
+
+// We use the documentRef objects to perform our CRUD methods (create, retrieve, update, delete).
+// The documentRef methods are .set(), .get(), .update() and .delete() respectively.
+
+// We can also add documents to collections using the collectionRef object using the .add() method
+// //collectionRef.add({value: prop})
+
+// We get the snapshotObject from the referenceObject using the .get() method. ie. documentRef.get()
+// or collectionRef.get()
+
+// documentRef returns a documentSnapshot object.
+// collectionRef returns a querySnapshot object.
+
+// userAuth is the object we get back from the google.auth when logging in
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    // get queryReference
+    const userRef = firestore.doc(`users/${userAuth.multiFactor.user.uid}`);
+
+    // get data on the user document
+    const snapShot = await userRef.get();
+
+    // if user doesn't exist in database, then create one
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData,
+            });
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+    return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
