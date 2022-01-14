22cf177bc55e9fbd0141ee6f0a98647bbb7f37d8
Mads Akselsen <akselsenmads@gmail.com>
2022-01-14 08:06:00 +0100

    adding redux 1

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        {/* Provider has access to the state, and has to be the parent of everything*/}
+        <Provider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
