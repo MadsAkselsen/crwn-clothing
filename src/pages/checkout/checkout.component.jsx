15aa477386c09947230e8b85800f33a0e8420418
Mads Akselsen <akselsenmads@gmail.com>
2022-01-16 11:30:59 +0100

    added checkout item

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 import {
     selectCartItems,
     selectCartTotal,
@@ -9,7 +10,6 @@ import {
 import './checkout.styles.scss';
 
 const CheckoutPage = ({ cartItems, total }) => {
-    console.log('cartItems', cartItems);
     return (
         <div className="checkout-page">
             <div className="checkout-header">
@@ -29,7 +29,9 @@ const CheckoutPage = ({ cartItems, total }) => {
                     <span>Remove</span>
                 </div>
             </div>
-            {cartItems.map((cartItem) => cartItem.name)}
+            {cartItems.map((cartItem) => (
+                <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+            ))}
 
             <div className="total">
                 <span>TOTAL: ${total}</span>
