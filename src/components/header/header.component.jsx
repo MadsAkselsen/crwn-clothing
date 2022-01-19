4bc5959978607b91809ef20eac3bc5d28be7d4bd
Mads Akselsen <akselsenmads@gmail.com>
2022-01-19 07:16:45 +0100

    adding styled components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 // ReactComponent is a speciel react syntax for importing SVGs
@@ -8,37 +7,35 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
 import { createStructuredSelector } from 'reselect';
 
-import './header.styles.scss';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selector';
+import {
+    HeaderContainer,
+    LogoContainer,
+    OptionDiv,
+    OptionLink,
+    OptionsContainer,
+} from './header.styles';
 
 const Header = ({ currentUser, hidden }) => (
-    <div className="header">
-        <Link className="logo-container" to="/">
-            <Logo className="logo" />
-        </Link>
-        <div className="options">
-            <Link className="option" to="/shop">
-                Shop
-            </Link>
-            <Link className="option" to="/contact">
-                CONTACT
-            </Link>
+    <HeaderContainer>
+        <LogoContainer to="/">
+            <Logo />
+        </LogoContainer>
+        <OptionsContainer>
+            <OptionLink to="/shop">SHOP</OptionLink>
+            <OptionLink to="/contact">CONTACT</OptionLink>
             {currentUser ? (
-                <div className="option" onClick={() => auth.signOut()}>
-                    SIGN OUT
-                </div>
+                <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
             ) : (
-                <Link className="option" to="/signin">
-                    SIGN IN
-                </Link>
+                <OptionLink to="/signin">SIGN IN</OptionLink>
             )}
             <CartIcon />
-        </div>
+        </OptionsContainer>
         {hidden ? null : <CartDropdown />}
-    </div>
+    </HeaderContainer>
 );
 
 // connects the store to the props
