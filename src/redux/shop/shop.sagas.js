75cda5a2d5a55964da060fe9a545b70b2a2d126a
Mads Akselsen <akselsenmads@gmail.com>
2022-01-24 07:03:51 +0100

    clear cart

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { call, put, takeEvery, takeLatest } from 'redux-saga/effects';
+import { all, call, put, takeEvery, takeLatest } from 'redux-saga/effects';
 import {
     convertCollectionsSnapshotToMap,
     firestore,
@@ -39,4 +39,8 @@ export function* fetchCollectionsStart() {
         ShopActionTypes.FETCH_COLLECTIONS_START,
         fetchCollectionAsync
     );
+}
+
+export function* shopSagas() {
+    yield all([call(fetchCollectionsStart)]);
 }
